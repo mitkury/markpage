7df52d5815558d52141825879b6df28e2dc84625
Dmitry Kury <dima.kurilchenko@gmail.com>
2025-09-04 09:44:11 +0500

    dev: remove unused SimpleMarkdownRenderer component

--- packages/markpage-svelte/src/lib/index.ts
+++ packages/markpage-svelte/src/lib/index.ts
@@ -4,7 +4,6 @@ export { ComponentParser } from './ComponentParser.js';
 
 // Svelte component exports
 export { default as MarkdownRenderer } from './MarkdownRenderer.svelte';
-export { default as SimpleMarkdownRenderer } from './SimpleMarkdownRenderer.svelte';
 
 // Type exports
 export type {
